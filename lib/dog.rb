8ce0b8aebc96e3c0a0ae1adc143ee03597b64619
fadimata tillman <fadi.tillman@gmail.com>
2020-04-14 01:29:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -88,4 +88,12 @@ class Dog
      self.new_from_db(row)
    end.first
  end
+ def update
+   sql = <<-SQL
+     UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+     SQL
+
+     DB[:conn].execute(sql, self.name, self.breed, self.id)
+ end
+
 end
