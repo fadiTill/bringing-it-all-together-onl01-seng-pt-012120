628a5d87368519327f311409653651deb327191e
fadimata tillman <fadi.tillman@gmail.com>
2020-04-14 01:28:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,4 +79,13 @@ class Dog
       end
       new_dog
   end
+  def self.find_by_name(name)
+   sql = <<-SQL
+     SELECT * FROM dogs WHERE name = ?
+   SQL
+
+   DB[:conn].execute(sql, name).map do |row|
+     self.new_from_db(row)
+   end.first
+ end
 end
